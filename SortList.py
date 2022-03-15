cc8b10b80ef4650786574d89bf31c8f323896771
Eric <jimladXLII@gmail.com>
2022-03-15 12:57:50 -0400

    initialization
    Added the original Sort List code, and added the list to be sorted.

---
+++ SortList.py
@@ -0,0 +1,41 @@
+def main():
+    #Variables
+    originalFile = open("Sort Me.txt", "r") #The original file we're getting our info from.
+    resultFile = open("Sorted List.txt", "w+") #The resulting file we're making.
+    list = originalFile.readlines() #The original list in array form.
+    finalList = [] #The final, resulting list.
+
+
+    #Clean the list of \n and spaces.
+    for i in range(len(list)):
+        list[i] = list[i].strip()
+
+    #(I was prepared to figure out an alphabetical sort, but I'll work with what I got.)
+    #Sort the list alphabetically
+    list = sorted(list)
+
+    #Sort by length, keeping the same, alphabetical, order.
+    while len(list) > 0:
+        #Make the variables
+        smallestValue = len(list[0]) #The size of the smallest string.
+        smallestIndex = 0 #The index of the smallest string.
+        #Search for the shortest string.
+        for j in range(len(list)):
+            if(smallestValue > len(list[j])):
+                smallestValue = len(list[j])
+                smallestIndex = j
+        #Stick the shortest at the end of the new list, removing it from the old list.
+        finalList.append(list.pop(smallestIndex))
+
+
+    #Output to the result file.
+    for line in finalList:
+        resultFile.write(line + "\n")
+
+    #Close our files.
+    resultFile.close()
+    originalFile.close()
+
+
+if __name__ == "__main__":
+    main()
